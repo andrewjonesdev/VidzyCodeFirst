48031f2f3185595a5d5913fb7b7912398139b125
Andrew Jones <jones.gaffney1920@gmail.com>
2017-11-08 16:49:24 -0600

    Preparing to add migration populating genres table

--- VidzyCodeFirst/Migrations/201711082220476_PopulateGenresTable.cs
+++ VidzyCodeFirst/Migrations/201711082220476_PopulateGenresTable.cs
@@ -7,22 +7,22 @@ namespace VidzyCodeFirst.Migrations
     {
         public override void Up()
         {
-            Sql("INSERT INTO Genres VALUES ('Comedy')");
-            Sql("INSERT INTO Genres VALUES ('Action')");
-            Sql("INSERT INTO Genres VALUES ('Horror')");
-            Sql("INSERT INTO Genres VALUES ('Thriller')");
-            Sql("INSERT INTO Genres VALUES ('Family')");
-            Sql("INSERT INTO Genres VALUES ('Romance')");
+            Sql("INSERT INTO Genres (Id, Name) VALUES (1, 'Comedy')");
+            Sql("INSERT INTO Genres (Id, Name) VALUES (2, 'Action')");
+            Sql("INSERT INTO Genres (Id, Name) VALUES (3, 'Horror')");
+            Sql("INSERT INTO Genres (Id, Name) VALUES (4, 'Thriller')");
+            Sql("INSERT INTO Genres (Id, Name) VALUES (5, 'Family')");
+            Sql("INSERT INTO Genres (Id, Name) VALUES (6, 'Romance')");
         }
         
         public override void Down()
         {
-            Sql("DELETE FROM Genres [WHERE Name ='Romance']");
-            Sql("DELETE FROM Genres [WHERE Name ='Family']");
-            Sql("DELETE FROM Genres [WHERE Name ='Thriller']");
-            Sql("DELETE FROM Genres [WHERE Name ='Horror']");
-            Sql("DELETE FROM Genres [WHERE Name ='Action']");
-            Sql("DELETE FROM Genres [WHERE Name ='Comedy']");
+            Sql("DELETE FROM Genres [WHERE Id=6 AND Name ='Romance']");
+            Sql("DELETE FROM Genres [WHERE Id=5 AND Name ='Family']");
+            Sql("DELETE FROM Genres [WHERE Id=4 AND Name ='Thriller']");
+            Sql("DELETE FROM Genres [WHERE Id=3 AND Name ='Horror']");
+            Sql("DELETE FROM Genres [WHERE Id=2 AND Name ='Action']");
+            Sql("DELETE FROM Genres [WHERE Id=1 AND Name ='Comedy']");
         }
     }
 }
