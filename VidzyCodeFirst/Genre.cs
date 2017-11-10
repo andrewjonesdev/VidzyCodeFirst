3e7f748c4fc7d0f3096562fb0d9ae0aea37f9880
Andrew Jones <jones.gaffney1920@gmail.com>
2017-11-09 21:00:53 -0600

    Broke Program File into its various classes

---
+++ VidzyCodeFirst/Genre.cs
@@ -0,0 +1,8 @@
+﻿namespace VidzyCodeFirst
+{
+    public class Genre
+    {
+        public byte Id { get; set; }
+        public string Name { get; set; }
+    }
+}
--- VidzyCodeFirst/Program.cs
+++ VidzyCodeFirst/Program.cs
@@ -1,40 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace VidzyCodeFirst
 {
-    public class Video
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public DateTime? ReleaseDate { get; set; }
-        public Genre Genre { get; set; }
-        public Classification Classification { get; set; }
-    }
-
-    public class Genre
-    {
-        public byte Id { get; set; }
-        public string Name { get; set; }
-    }
-
-    public class VidzyContext : DbContext
-    {
-        public DbSet<Video> Videos { get; set; }
-        public DbSet<Genre> Genres { get; set; }
-    }
-
-    public enum Classification
-    {
-        Silver = 1,
-        Gold = 2,
-        Platinum = 3
-    }
-
     public class Program
     {
         static void Main(string[] args)
